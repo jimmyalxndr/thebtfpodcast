4255614be98deb5a7c65811eb930f5c81eaf8611
jimmyalxndr <jimmyalexandercontact@gmail.com>
2026-09-03 14:36:11 +1000

    Add site config and builder for Netlify

---
+++ site_config.py
@@ -0,0 +1,24 @@
+RSS_URL = "https://media.rss.com/behind-the-facade/feed.xml"
+COVER_URL = "https://media.rss.com/behind-the-facade/20260317_040315_f2ea699a2cd1b7e52792616acc457e17.jpg"
+SITE = "https://thebtfpodcast.com"
+YT = "https://www.youtube.com/@BehindTheFacadeShow"
+ITUNES = "{http://www.itunes.com/dtds/podcast-1.0.dtd}"
+
+SHOW = {
+    "title": "Behind the Facade",
+    "tagline": "Real conversations about construction from the people who live it every day.",
+    "description": (
+        "A monthly construction podcast hosted by Jake Gorry, Director of Proline Group, "
+        "and Daniel Alizzi of Complex Facade Install."
+    ),
+    "spotify": "https://open.spotify.com/show/69VaZ25YNDGeuHekiEhoJS",
+    "apple": "https://podcasts.apple.com/au/podcast/behind-the-facade/id1884543390",
+    "youtube": YT,
+    "amazon": "https://music.amazon.com.au/podcasts/a79403c0-94b3-492c-b2d3-9aa3441727cf/behind-the-facade",
+    "rss": RSS_URL,
+    "instagram": "https://www.instagram.com/behindthefacadeshow/",
+    "linkedin": "https://www.linkedin.com/company/behind-the-facade-show/",
+    "jake": "https://www.linkedin.com/in/jake-gorry-87b30628b/",
+    "daniel": "https://www.linkedin.com/in/daniel-alizzi-867595376/",
+    "proline": "https://www.prolinemetalcladding.com.au/",
+}
